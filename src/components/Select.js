63922869d31a3d5d231141817fe863653e810ebf
Vinicius <disanti2010@gmail.com>
2018-07-02 00:16:12 -0300

    refactor: componentizando o select dos livros
    O select era utilizado mais de uma vez em toda a aplicação, então foi necessário componentizá-lo para reuso.

---
+++ src/components/Select.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Select extends Component {
+    static propTypes = {
+        label: PropTypes.object,
+        options: PropTypes.array.isRequired
+    }
+
+    render() {
+        const {label, options} = this.props;
+        return ( <select>
+            <option value={label.value} disabled>{label.description}</option>
+            {options.map((option) => (
+                <option value={option.value}>{option.description}</option>
+            ))}
+        </select>
+        )
+    }
+}
+
+export default Select;
