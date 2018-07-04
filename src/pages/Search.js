54a0589d10ef1f6762e176e0d9ba87d0bc69b1b6
Vinicius <disanti2010@gmail.com>
2018-07-03 22:18:04 -0300

    feat: Buscando livro ao pesquisar e adicionando na prateleira.
    Ao realizar a busca na tela de busca de livros, a API retornará os livros de acordo com o que foi digitado e, ao mudar o status, irá para a prateleira da página inicial

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import Book from "../components/Book";
 
 export default class Search extends Component {
+  state = {
+    query: null,
+    books: []
+  };
+
+  searchBooks = event => {
+    const query = event.target.value;
+    this.setState({
+      query
+    });
+    if (!query) {
+      this.setState({
+        books: []
+      });
+      return false;
+    }
+
+    BooksAPI.search(this.state.query, 20).then(books => {
+      this.setState({
+        books
+      });
+    });
+  };
+
+  changeMethodStatus = (book, shelf) => {
+    BooksAPI.update(book, shelf);
+  };
+
   render() {
+    const validateBooksPresence = () => {
+      return this.state.books && !this.state.books.error;
+    };
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -19,11 +53,27 @@ export default class Search extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              onChange={this.searchBooks}
+              placeholder="Search by title or author"
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          <ol className="books-grid">
+            {validateBooksPresence() &&
+              this.state.books.map((book, index) => {
+                return (
+                  <li key={index}>
+                    <Book
+                      book={book}
+                      onChangeStatus={this.changeMethodStatus}
+                    />
+                  </li>
+                );
+              })}
+          </ol>
         </div>
       </div>
     );
